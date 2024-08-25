7a6d345824c59140c7f2d957b81522cc242e8c32
Andree55 <adrianwie55@gmail.com>
2024-08-26 00:17:02 +0200

    Add Data Model

---
+++ CoachBuddy.Domain/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoachBuddy.Domain.Entities
+{
+    public class Client
+    {
+        public required int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ClientContactDetails ContactDetails { get; set; } = default!;
+        public string EncodedName { get; private set; } = default!;
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ", "-");
+    }
+}
