4bb5a445ff1bf6a298802386dc77014e46cd0282
Andree55 <adrianwie55@gmail.com>
2024-08-26 11:55:36 +0200

    Configure connection with database

---
+++ CoachBuddy.Infrastructure/Extensions/ServiceCollecionExtension.cs
@@ -0,0 +1,21 @@
+﻿using CoachBuddy.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoachBuddy.Infrastructure.Extensions
+{
+    public static class ServiceCollecionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services,IConfiguration configuration) 
+        {
+            services.AddDbContext<CoachBuddyDbContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("CoachBuddy")));
+        }
+    }
+}
--- CoachBuddy.Infrastructure/Persistence/CoachBuddyDbContext.cs
+++ CoachBuddy.Infrastructure/Persistence/CoachBuddyDbContext.cs
@@ -9,6 +9,15 @@ namespace CoachBuddy.Infrastructure.Persistence
 {
     public class CoachBuddyDbContext:DbContext
     {
+        public CoachBuddyDbContext(DbContextOptions<CoachBuddyDbContext> options):base(options)
+        {
+            
+        }
         public DbSet<Domain.Entities.Client> Clients { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.Client>()
+                .OwnsOne(c => c.ContactDetails);
+        }
     }
 }
