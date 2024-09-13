d7e1524ebd3d49dbeeca7a89753c7b78aa95bdca
Andree55 <adrianwie55@gmail.com>
2024-09-13 16:01:14 +0200

    Add dynamic data rendering

---
+++ CoachBuddy.Application/ClientTraining/Queries/GetClientTrainings/GetClientTrainingsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CoachBuddy.Domain.Interfaces;
+using MediatR;
+
+
+namespace CoachBuddy.Application.ClientTraining.Queries.GetClientTrainings
+{
+    public class GetClientTrainingsQueryHandler : IRequestHandler<GetClientTrainingsQuery, IEnumerable<ClientTrainingDto>>
+    {
+        private readonly IClientTrainingRepository _clientRepository;
+        private readonly IMapper _mapper;
+
+        public GetClientTrainingsQueryHandler(IClientTrainingRepository clientRepository,IMapper mapper)
+        {
+            _clientRepository = clientRepository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ClientTrainingDto>> Handle(GetClientTrainingsQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _clientRepository.GetAllByEncodedName(request.EncodedName);
+
+            var dtos= _mapper.Map<IEnumerable<ClientTrainingDto>>(result);
+
+            return dtos;
+        }
+    }
+}
--- CoachBuddy/wwwroot/js/Client/Edit.js
+++ CoachBuddy/wwwroot/js/Client/Edit.js
@@ -1,4 +1,40 @@
 ﻿$(document).ready(function () {
+
+    const RenderClientServices = (trainings, container) => {
+        container.empty();
+
+        for (const training of trainings) {
+            container.append(
+                `<div class="card border-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">${training.date}</div>
+          <div class="card-body">
+            <h5 class="card-title">${training.description}</h5> 
+          </div>
+        </div>`)
+        }
+    }
+
+    const LoadClientTrainings = () => {
+        const container = $("#services")
+        const clientEncodedName = container.data("encodedName");
+
+        $.ajax({
+            url: '/Client/${clientEncodedName}/ClientTraining',
+            type: 'get',
+            success: function (data) {
+                if (!data.length) {
+                    container.html("There are no trainings for this client")
+                } else {
+                    RenderClientTrainings(data, container)
+                }
+            },
+            error: function () {
+                toastr["error"]("Something went wrong")
+            }
+        })
+    }
+
+    LoadClientTrainings();
     $("#createClientTrainingModal form").submit(function (event) {
         event.preventDefault();
 
