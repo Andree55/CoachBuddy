b872d15b43981bbe987dd5d4fe6d1914dbbd80cc
Andree55 <adrianwie55@gmail.com>
2024-09-15 14:15:42 +0200

    Set Date to store only the date

--- CoachBuddy.Application/ClientTraining/ClientTrainingDto.cs
+++ CoachBuddy.Application/ClientTraining/ClientTrainingDto.cs
@@ -9,6 +9,6 @@ namespace CoachBuddy.Application.ClientTraining
     public class ClientTrainingDto
     {
         public string Description { get; set; } = default!;
-        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public DateTime Date { get; set; } = DateTime.UtcNow.Date;
     }
 }
--- CoachBuddy.Domain/Entities/ClientTraining.cs
+++ CoachBuddy.Domain/Entities/ClientTraining.cs
@@ -10,7 +10,7 @@ namespace CoachBuddy.Domain.Entities
     {
         public int Id  { get; set; }
         public string Description { get; set; } = default!;
-        public DateTime Date { get; set; } = default!;
+        public DateTime Date { get; set; } = DateTime.UtcNow.Date;
 
         public int ClientId { get; set; } = default!;
         public Client Client { get; set; } = default!;
