2f1d37e0c74fee85f30513a2ec40da8f3954e4a2
Andree55 <adrianwie55@gmail.com>
2024-09-29 17:53:25 +0200

    Add pagination to index view

--- CoachBuddy.Application/Common/PaginatedResult.cs
+++ CoachBuddy.Application/Common/PaginatedResult.cs
@@ -16,5 +16,6 @@ namespace CoachBuddy.Application.Common
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
         public bool HasPreviousPage => PageNumber > 1;
         public bool HasNextPage => PageNumber < TotalPages;
+
     }
 }
