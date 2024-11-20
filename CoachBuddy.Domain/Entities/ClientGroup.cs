98b29a503cfc560e4a03b642cd1a0ab5933e6a1a
Andree55 <adrianwie55@gmail.com>
2024-11-20 19:24:22 +0100

    Add group entities

---
+++ CoachBuddy.Domain/Entities/ClientGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace CoachBuddy.Domain.Entities
+{
+    public class ClientGroup
+    {
+        public int ClientId { get; set; }
+        public Client Client { get; set; } = default!;
+        public int GroupId { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ CoachBuddy.Domain/Entities/Group.cs
@@ -0,0 +1,12 @@
+﻿namespace CoachBuddy.Domain.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedA { get; set; } = DateTime.UtcNow;
+
+        public List<ClientGroup> ClientGroups { get; set; } = new();
+    }
+}
