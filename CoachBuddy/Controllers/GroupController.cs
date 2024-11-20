4031db80124d4fb1fdc205166b06e38e317280ad
Andree55 <adrianwie55@gmail.com>
2024-11-20 22:57:06 +0100

    Add group view

---
+++ CoachBuddy/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using CoachBuddy.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoachBuddy.MVC.Controllers
+{
+    public class GroupController:Controller
+    {
+        private readonly CoachBuddyDbContext _context;
+        public GroupController(CoachBuddyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var groups = await _context.Groups.ToListAsync();
+            return View(groups);
+        }
+    }
+}
