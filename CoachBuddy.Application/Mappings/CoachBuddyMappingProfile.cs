cbfa836a307a346f9eb74beabed6eede6b999982
Andree55 <adrianwie55@gmail.com>
2024-12-14 15:31:51 +0100

    Add exercise count to the dashboard

--- CoachBuddy.Application/Mappings/CoachBuddyMappingProfile.cs
+++ CoachBuddy.Application/Mappings/CoachBuddyMappingProfile.cs
@@ -6,6 +6,8 @@ using CoachBuddy.Application.Client.Commands.EditClient;
 using CoachBuddy.Application.ClientGroup;
 using CoachBuddy.Application.ClientTraining;
 using CoachBuddy.Application.Exercise;
+using CoachBuddy.Application.Exercise.Commands.CreateExercise;
+using CoachBuddy.Application.Exercise.Commands.EditExercise;
 using CoachBuddy.Application.Group;
 using CoachBuddy.Application.Group.Commands.CreateGroup;
 using CoachBuddy.Application.Group.Commands.EditGroup;
@@ -88,7 +90,17 @@ namespace CoachBuddy.Application.Mappings
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.Client.Name} {src.Client.LastName}"));
 
             CreateMap<Domain.Entities.Exercise.Exercise, ExerciseDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.MuscleGroup, opt => opt.MapFrom(src => src.MuscleGroup))
                 .ForMember(dest => dest.IsEditable, opt => opt.MapFrom(src => user != null && user.IsInRole("Admin")));
+
+            CreateMap<CreateExerciseCommand, Domain.Entities.Exercise.Exercise>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.MuscleGroup, opt => opt.MapFrom(src => src.MuscleGroup));
+
+            CreateMap<ExerciseDto, EditExerciseCommand>();
         }
     }
 }
