e7fc2f11bfb294ecba377d74b0d575f976ded45b
Andree55 <adrianwie55@gmail.com>
2024-12-20 09:15:50 +0100

    Add edit command

---
+++ CoachBuddy.Application/TrainingPlan/Commands/EditTrainingPlan/EditTrainingPlanCommand.cs
@@ -0,0 +1,10 @@
+﻿using CoachBuddy.Application.Exercise;
+using MediatR;
+
+namespace CoachBuddy.Application.TrainingPlan.Commands.EditTrainingPlan
+{
+    public class EditTrainingPlanCommand:ExerciseDto,IRequest
+    {
+
+    }
+}
---
+++ CoachBuddy.Application/TrainingPlan/Commands/EditTrainingPlan/EditTrainingPlanCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace CoachBuddy.Application.TrainingPlan.Commands.EditTrainingPlan
+{
+    public class EditTrainingPlanCommandHandler : IRequestHandler<EditTrainingPlanCommand>
+    {
+        public Task<Unit> Handle(EditTrainingPlanCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
