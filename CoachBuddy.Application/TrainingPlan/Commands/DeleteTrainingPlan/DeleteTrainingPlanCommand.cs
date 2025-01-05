b782a3a522fb46a3083874ec5ebe6b4831856f28
Andree55 <adrianwie55@gmail.com>
2025-01-05 22:28:14 +0100

    Add groups to details

--- CoachBuddy.Application/TrainingPlan/Commands/DeleteTrainingPlan/DeleteTrainingPlanCommand.cs
+++ CoachBuddy.Application/TrainingPlan/Commands/DeleteTrainingPlan/DeleteTrainingPlanCommand.cs
@@ -2,7 +2,7 @@
 
 namespace CoachBuddy.Application.TrainingPlan.Commands.DeleteTrainingPlan
 {
-    public class DeleteTrainingPlanCommand:TrainingPlanDto,IRequest
+    public class DeleteTrainingPlanCommand : TrainingPlanDto, IRequest
     {
         public int Id { get; set; }
     }
